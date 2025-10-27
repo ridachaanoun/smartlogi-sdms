a277a2fdd49ddacb73c9be8a1386dfcb9709aa3b
ridachaanoun <ridachaanoun.ff.2@gmail.com>
2025-10-27 11:28:33 +0100

    Initialize Smart Delivery Management System project with Maven setup and basic application structure

---
+++ src/main/java/com/smartlogi/sdms/SmartDeliveryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.smartlogi.sdms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDeliveryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDeliveryManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartlogi/sdms/SmartDeliveryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartlogi.sdms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDeliveryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
