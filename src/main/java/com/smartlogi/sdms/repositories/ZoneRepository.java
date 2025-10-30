15bd091d3fdd0ae7befa43b0e4d57b939f26db31
ridachaanoun <ridachaanoun.ff.2@gmail.com>
2025-10-30 17:17:46 +0100

    Add Spring Boot validation and actuator dependencies; create ZoneRepository interface

--- src/main/java/com/smartlogi/sdms/repositories/ZoneRepository.java
+++ src/main/java/com/smartlogi/sdms/repositories/ZoneRepository.java
@@ -1,7 +1,8 @@
 package com.smartlogi.sdms.repositories;
 
 import com.smartlogi.sdms.entities.Delivery;
+import com.smartlogi.sdms.entities.Zone;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WoneRepository extends JpaRepository<Delivery, String> {
+public interface ZoneRepository extends JpaRepository<Zone, String> {
 }
